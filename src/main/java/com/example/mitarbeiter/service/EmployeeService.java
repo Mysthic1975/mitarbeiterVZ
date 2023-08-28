f642baf268f6ef2fbec9c55f195c66aac7233cac
Mysthic1975 <108031921+Mysthic1975@users.noreply.github.com>
2023-08-28 13:34:09 +0200

    edit.html // 2 Fehlmeldungen ausgeblendet

--- src/main/java/com/example/mitarbeiter/service/EmployeeService.java
+++ src/main/java/com/example/mitarbeiter/service/EmployeeService.java
@@ -7,11 +7,18 @@ import org.springframework.web.multipart.MultipartFile;
 
 public interface EmployeeService {
     Page<EmployeeEntity> getAllEmployees(int page, int size);
+
     Page<EmployeeEntity> getAllEmployeesSortedBy(String sortBy, String sortDirection, int page, int size);
+
     EmployeeEntity getEmployeeById(Long employeeId);
+
     Page<EmployeeEntity> searchEmployees(String search, Pageable pageable);
+
     void saveEmployee(EmployeeEntity employee);
+
     void updateEmployee(EmployeeEntity employee, Long employeeId, MultipartFile pictureFile);
+
     void deleteEmployee(Long employeeId);
+
     void deleteEmployeePicture(Long id);
 }
--- src/main/java/com/example/mitarbeiter/service/PositionService.java
+++ src/main/java/com/example/mitarbeiter/service/PositionService.java
@@ -1,13 +1,18 @@
 package com.example.mitarbeiter.service;
 
 import com.example.mitarbeiter.entity.PositionEntity;
+
 import java.util.List;
 
 public interface PositionService {
     List<PositionEntity> getAllPositions();
+
     PositionEntity getPositionById(Long id);
+
     void savePosition(PositionEntity position);
+
     void updatePosition(PositionEntity position, Long id);
+
     void deletePosition(Long id);
 }
 
