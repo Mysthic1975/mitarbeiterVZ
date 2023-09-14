0cb60cc9f24fecd499ff9392b4509bc1c449811f
Mysthic1975 <108031921+Mysthic1975@users.noreply.github.com>
2023-09-14 17:41:00 +0200

    design

---
+++ src/main/java/org/thymeleaf/expression/ExpressionRoot.java
@@ -0,0 +1,9 @@
+package org.thymeleaf.expression;
+
+import org.springframework.expression.ExpressionParser;
+import org.thymeleaf.context.ExpressionContext;
+
+public class ExpressionRoot {
+    public ExpressionRoot(ExpressionParser parser, ExpressionContext expressionContext) {
+    }
+}
